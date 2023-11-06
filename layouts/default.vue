f7d2cf0f6bc6244059fd3cc7c30b15cb55073a62
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-11-06 12:36:30 -0500

    [feat]: added favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,16 @@
 <script setup>
+import { computed } from "vue";
 import NavHeader from "../components/ui/AppNav.vue";
 import Footer from "../components/ui/AppFooter.vue";
+import { useFavicon, usePreferredDark } from "@vueuse/core";
+
+const isDark = usePreferredDark();
+
+const favicon = computed(() =>
+  isDark.value ? "/favicon_dark.svg" : "/favicon.svg"
+);
+
+const icon = useFavicon(favicon);
 </script>
 <template>
   <main class="overflow-x-hidden scroll-smooth">
