1f763cb755a200ca9ae55ce9988bb15895e18f96
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-11-06 15:05:43 -0500

    cleaning

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,6 @@ import LandingBg from "../assets/landingBG.svg";
 import Button from "../components/ui/Button/PrimaryButton.vue";
 import Faq from "../components/TheFaq.vue";
 
-// TODO: card-fixes has commits
-
 const cardTitles = [
   {
     id: 1,
