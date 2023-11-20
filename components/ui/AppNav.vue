4b967862a8d0b096843d1ecea6a19d7361bb7d99
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-11-19 23:38:29 -0500

    [feat]: added meta stuff & event listeners for nav

--- components/ui/AppNav.vue
+++ components/ui/AppNav.vue
@@ -1,15 +1,5 @@
-<script setup>
-import Logo from "./AppLogo.vue";
-import NavLine from "~/assets/navLine.svg";
-import { Icon } from "@iconify/vue";
-import NavMobile from "./AppNavMobile.vue";
-import Button from "./Button/PrimaryButton.vue";
-
-const isOpen = ref(false);
-</script>
-
 <template>
-  <nav class="absolute z-50 top-0 left-0 right-0 py-4">
+  <nav class="absolute z-50 top-0 left-0 right-0 py-4" ref="container">
     <div class="relative container flex justify-between items-center">
       <div class="flex-1">
         <nuxt-link to="/">
@@ -52,6 +42,53 @@ const isOpen = ref(false);
       />
     </div>
 
-    <NavMobile v-bind:isOpen="isOpen" />
+    <Transition name="slide-fade">
+      <NavMobile v-if="isOpen" v-bind:isOpen="isOpen" />
+    </Transition>
   </nav>
 </template>
+
+<script setup>
+import Logo from "./AppLogo.vue";
+import NavLine from "~/assets/navLine.svg";
+import { Icon } from "@iconify/vue";
+import NavMobile from "./AppNavMobile.vue";
+import Button from "./Button/PrimaryButton.vue";
+import { useMediaQuery } from "@vueuse/core";
+
+const isMobile = useMediaQuery("(max-width: 1024px)");
+
+const isOpen = ref(false);
+const container = ref();
+
+const clickOutsideHandler = (event) => {
+  if (
+    container.value &&
+    !container.value.contains(event.target) &&
+    isOpen.value
+  ) {
+    isOpen.value = false;
+  }
+};
+
+const escapeKeyHandler = (event) => {
+  if (event.key === "Escape" && isOpen.value) {
+    isOpen.value = false;
+  }
+};
+
+onMounted(() => {
+  document.addEventListener("click", clickOutsideHandler);
+  document.addEventListener("keydown", escapeKeyHandler);
+  window.addEventListener("resize", () => {
+    if (isMobile.value) {
+      isOpen.value = false;
+    }
+  });
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener("click", clickOutsideHandler);
+  document.removeEventListener("keydown", escapeKeyHandler);
+});
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,56 @@ import NavHeader from "../components/ui/AppNav.vue";
 import Footer from "../components/ui/AppFooter.vue";
 import { useFavicon, usePreferredDark } from "@vueuse/core";
 
+useHead({
+  title: "Shadowfall",
+  meta: [
+    { name: "Shadowfall", content: "Shadowfall" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "description", content: "Shadowfall" },
+    { name: "og:title", content: "Shadowfall" },
+    { name: "og:description", content: "Shadowfall" },
+    { name: "og:image", content: "Shadowfall" },
+    { name: "og:url", content: "Shadowfall" },
+    { name: "twitter:card", content: "Shadowfall" },
+    { name: "twitter:site", content: "Shadowfall" },
+    { name: "twitter:title", content: "Shadowfall" },
+    { name: "twitter:description", content: "Shadowfall" },
+    { name: "twitter:image", content: "Shadowfall" },
+    { name: "twitter:url", content: "Shadowfall" },
+    { name: "twitter:creator", content: "Shadowfall" },
+    { name: "theme-color", content: "#000000" },
+    { name: "msapplication-TileColor", content: "#000000" },
+    { name: "msapplication-TileImage", content: "/ms-icon-144x144.png" },
+    { name: "msapplication-config", content: "/browserconfig.xml" },
+    { name: "apple-mobile-web-app-title", content: "Shadowfall" },
+    { name: "application-name", content: "Shadowfall" },
+    { name: "msapplication-tooltip", content: "Shadowfall" },
+    { name: "msapplication-starturl", content: "/" },
+    { name: "msapplication-tap-highlight", content: "no" },
+    { name: "full-screen", content: "yes" },
+    { name: "browsermode", content: "application" },
+    { name: "nightmode", content: "enable/disable" },
+    { name: "layoutmode", content: "fitscreen/standard" },
+    { name: "imagemode", content: "force" },
+    { name: "screen-orientation", content: "portrait" },
+    { name: "x5-orientation", content: "portrait" },
+    { name: "browsermode", content: "application" },
+    { name: "x5-page-mode", content: "app" },
+    { name: "msapplication-navbutton-color", content: "#000000" },
+    { name: "apple-mobile-web-app-status-bar-style", content: "#000000" },
+    { name: "apple-mobile-web-app-capable", content: "yes" },
+    { name: "apple-touch-fullscreen", content: "yes" },
+    { name: "apple-mobile-web-app-title", content: "Shadowfall" },
+    { name: "mobile-web-app-capable", content: "yes" },
+    { name: "HandheldFriendly", content: "true" },
+    { name: "MobileOptimized", content: "320" },
+    { name: "mobile-web-app-capable", content: "yes" },
+    { name: "format-detection", content: "telephone=no" },
+    { name: "apple-mobile-web-app-capable", content: "yes" },
+    { name: "apple-mobile" },
+  ],
+});
+
 const isDark = usePreferredDark();
 
 const favicon = computed(() =>
